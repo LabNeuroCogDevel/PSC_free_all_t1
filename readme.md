5eb40aa5e06caa3ae04a42941a3b18c41969e826
Will Foran <willforan@gmail.com>
2018-08-09 13:16:11 -0400

    run missing again

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ using the longitudinal  pipeline on the Pittsburgh Super Computer (XSEDE).
   0. collect and transfer. see `tx/`
   1. first pass FS: `run_t1_all.bash` submits `fs_batch_cmd.bash` jobs to with `sbatch`
   2. long pass: `run_long.bash` submits `fs_batch_long.bash` 
-  3. copy from final dest (pittcompgen): `rsync -azvhi foranw@bridges.psc.xsede.org:~/scratch/FS/ /data2/MRI/Enigma_FS_long`
+  3. copy from final dest (pittcompgen): `rsync -azvhi foranw@bridges.psc.xsede.org:~/scratch/FS/ /data2/MRI/CogLong/Freesurfer`
 
 ## ref
 
--- run_redo_longbase.bash
+++ run_redo_longbase.bash
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+
+#
+# find incomplete long runs and rerun
+#
+
 export SUBJECTS_DIR=$SCRATCH/FS
+logfile="$SCRATCH/joblog/long-%A_%a.out" 
+
 grep -Li 'finished without e' scratch/FS/*long.base*/scripts/recon-all.log |
  perl -lne 'print "$1 $2" if m/((\d{5})_\d+)/' |
  while read iddate base; do
@@ -10,12 +17,18 @@ grep -Li 'finished without e' scratch/FS/*long.base*/scripts/recon-all.log |
   
    # rerun making base
    if ! grep 'finished without err' -iq $blog; then
-     echo base_$SUBJ did not finish
-     [ $(squeue -A $(id -gn) -n base_$SUBJ | wc -l) -gt 1 ] && echo "$SUBJ: already in queue!" && continue
+     #echo base_$SUBJ did not finish
+     [ $(squeue -A $(id -gn) -n base_$SUBJ | wc -l) -gt 1 ] && echo "base_$SUBJ: already in queue!" && continue
+
+     #echo -n "SUBJ=$SUBJ "
      echo sbatch -o $logfile -e $logfile -J base_$SUBJ fs_batch_long.bash 
      continue
    fi
   
-   echo recon-all -long $iddate base_$SUBJ  -all -no-isrunning
+   export LONG=$iddate
+   #echo -n "LONG=$LONG  SUBJ=$SUBJ "
+
+   [ $(squeue -A $(id -gn) -n long_$LONG | wc -l) -gt 1 ] && echo "long_$LONG: already in queue!" && continue
+   echo sbatch -o $logfile -e $logfile -J long_$LONG fs_batch_redo_long.bash
 done
 
